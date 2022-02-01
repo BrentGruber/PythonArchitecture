3945d823258cf993c66ef7e793a1368e8e48dafb
BrentGruber <brent.gruber77@gmail.com>
2022-02-01 00:05:02 -0500

    adding deallocate feature

--- 001-Domain-Modeling/models.py
+++ 001-Domain-Modeling/models.py
@@ -16,7 +16,56 @@ class Batch:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set() # Set allocations to an empty set
 
-    def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+    def can_allocate(self, line: OrderLine) -> bool:
+        """Evaluates whether an order line can be allocated against this batch.
+
+        Args:
+            line (OrderLine): Order line containing a sku and requested quantity
+
+        Returns:
+            bool: True if all conditions are met to fulfill order, False otherwise
+        """
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def allocate(self, line: OrderLine) -> None:
+        """Allocate an order on the current batch
+
+        Args:
+            line (OrderLine): Order line containing requested items
+        """
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine) -> None:
+        """Deallocates a previously allocated OrderLine
+
+        Args:
+            line (OrderLine): allocated order line of which to deallocate
+        """
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    #@property becomes a calculated field in the classe
+    @property
+    def allocated_quantity(self) -> int:
+        """Current quantity allocated to orders
+
+        Returns:
+            int: number of items allocated
+        """
+        return sum(line.qty for line in self._allocations)
+
+    @property 
+    def available_quantity(self) -> int:
+        """Current quantity available for allocation
+
+        Returns:
+            int: current number of items available for allocation
+        """
+        return self._purchased_quantity - self.allocated_quantity
+    
+
+    
--- 001-Domain-Modeling/tests/test_batches.py
+++ 001-Domain-Modeling/tests/test_batches.py
@@ -14,34 +14,37 @@ def make_batch_and_line(sku, batch_qty, line_qty):
     )
 
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    """
-    Allocating an orderline should reduce the batch quantity
-    """
     batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
     batch.allocate(line)
 
     assert batch.available_quantity == 18
 
 def test_can_allocate_if_available_greater_than_required():
-    """
-    OrderLine cannot be allocated if there is not enough batch quantity
-    """
     batch, line = make_batch_and_line("ELEGENT-LAMP", 20, 2)
+
     assert batch.can_allocate(line)
 
 def test_cannot_allocate_if_available_smaller_than_required():
     batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
+
     assert batch.can_allocate(line) is False
 
 def test_can_allocate_if_available_equal_to_required():
     batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
+    
     assert batch.can_allocate(line)
 
 def test_cannot_allocate_if_skus_dont_match():
-    batch = Batch("batch-001", "ELEGANT-LAMP", 20, eta=today),
+    batch = Batch("batch-001", "ELEGANT-LAMP", 20, eta=today)
     line = OrderLine("order-123", "BLUE-CUSHION", 3)
 
     assert batch.can_allocate(line) is False
+
+def test_can_only_deallocated_allocated_lines():
+    batch, line = make_batch_and_line("DECORATIVE_TRINKET", 20, 2)
+    batch.deallocate(line)
+
+    assert batch.available_quantity == 20
 
 # def test_prefers_warehouse_batches_to_shipments():
 #     pytest.fail("todo")
