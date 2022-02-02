618c4fb7f6c52beda325d9429ce1ff0be661a8f1
BrentGruber <brent.gruber77@gmail.com>
2022-02-02 08:42:12 -0500

    finishing chapter 2

---
+++ 002-Repository-Model/repository.py
@@ -0,0 +1,40 @@
+import abc
+
+import models
+
+class AbstractRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def add(self, batch: models.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference) -> models.Batch:
+        raise NotImplementedError
+
+class SqlAlchemyRepository(AbstractRepository):
+
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(models.Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(models.Batch).all()
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches):
+        self._batches = set(batches)
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return list(self._batches)
---
+++ 002-Repository-Model/tests/test_repository.py
@@ -0,0 +1,35 @@
+import pytest
+
+def test_repository_can_save_a_batch(session):
+    batch = model.Batch("batch1", "RUSTY-SOAPDISH", 100, eta=None)
+
+    repo = repository.SqlAlchemyRepository(session)
+    repo.add(batch)
+    session.commit()
+
+    rows = list(session.execute(
+        'SELECT reference, sku, _purchased_quantity, eta from "batches"'
+    ))
+    assert rows == [("batch1", "RUSTY-SOAPDISH", 100, None)]
+
+def insert_order_line(session):
+    session.execute(
+        'INSERT INTO order_lines (orderid, sku, qty)'
+        ' VALUES ("order1", "GENERIC-SOFA", 12)'
+    )
+    [[orderline_id]] = session.execute(
+        'SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku',
+        dict(orderid="order1", sku="GENERIC-SOFA")
+    )
+    return orderline_id
+
+def insert_batch(session, batch_id):
+    session.execute(
+        'INSERT INTO batches (orderid, sku, qty)'
+        ' VALUES ("order1", "GENERIC-SOFA", 12)'
+    )
+    [[orderline_id]] = session.execute(
+        'SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku',
+        dict(orderid="order1", sku="GENERIC-SOFA")
+    )
+    return orderline_id
